5baea842fc26d27075ca70adeda7bd2319ba22fd
theVinesh <hi@thevinesh.com>
2020-12-30 15:14:06 +0530

    Update bloc version

--- lib/app/home/bloc/home_bloc.dart
+++ lib/app/home/bloc/home_bloc.dart
@@ -2,16 +2,17 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:splash/app/home/bloc/home_event.dart';
 import 'package:splash/app/home/bloc/home_state.dart';
-import 'package:splash/app/model/photo_model.dart';
 import 'package:splash/app/home/repository/photos_repository.dart';
+import 'package:splash/app/model/photo_model.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final PhotosRepository repository;
 
-  HomeBloc({@required this.repository}) : assert(repository != null);
+  HomeBloc({@required this.repository})
+      : assert(repository != null),
+        super(Empty());
 
-  @override
-  HomeState get initialState => Empty();
+
 
   @override
   Stream<HomeState> mapEventToState(HomeEvent event) async* {
